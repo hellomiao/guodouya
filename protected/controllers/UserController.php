fd19058e6965bd6d0c304ab2954e4aac1ccb92fc
yang <yang@yang-PC>
2013-03-15 11:33:06 +0800

    main

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -1,6 +1,31 @@
 <?php
-class UserController extends Controller{
-    public function actionSetting(){
+
+class UserController extends Controller {
+
+    public function filters() {
+        return array(
+            'accessControl', // perform access control for CRUD operations
+        );
+    }
+
+    public function accessRules() {
+        return array(
+            array('allow', // allow all users to access 'index' and 'view' actions.
+                'actions' => array('index', 'view'),
+                'users' => array('*'),
+            ),
+            array('allow', // allow authenticated users to access all actions
+                'users' => array('@'),
+            ),
+            array('deny', // deny all users
+                'users' => array('*'),
+            ),
+        );
+    }
+
+    public function actionSetting() {
+        echo $nickname = Yii::app()->user->name;
         $this->render("setting");
     }
+
 }
---
+++ protected/views/user/setting.php
@@ -0,0 +1,89 @@
+<div id="content" class="container" style="width: 70%;">
+
+    <div class="container-fluid">
+        <div class="row-fluid">
+            
+            <div class="span9 plant-right">
+                
+            
+
+<div id="personinfo">
+<div class="settings">吃饭撑死的设置</div>
+<div class="photo_p">
+  <p class="pull-left"><img width="60" height="60" id="crop_preview" name="crop_preview" src="http://project.hcg.com/files/Avatar/3/30/15.jpg"></p>
+  <div class="choose_p">
+  <div>
+      <iframe width="0" height="0" frameborder="0" id="uploadface" name="uploadface" marginwidth="0" src="about:blank"></iframe>
+   <a class="btn_upload" style="position:relative" href="javascript:;">选择新头像
+       <form method="post" action="/user/AjaxUpload" enctype="multipart/form-data" name="face_form" target="uploadface" id="face_form"><input type="file" name="face" onchange="document.getElementById('face_form').submit();" class="file_formupoadfile " id="idFile"></form> 
+   </a>   
+</div>
+    <input type="hidden" id="img_path">
+   <p>你可以选择png/jpg图片(100*100)作为头像 </p>
+  </div>
+</div>
+
+<div>
+    <div class="profile_set tn-form ">
+        <div class="eidt_pros">
+
+            <div class="tn-form-row ">
+
+                <p><label class="tn-form-label">
+
+                    邮箱
+              </label></p>
+
+                <input type="text" class="input-large" value="1@qq.com" disable="" readonly="true" maxlength="20">
+
+
+
+            </div>
+
+            <div class="tn-form-row ">
+
+                <p><label class="tn-form-label">
+
+                    昵称</label></p>
+
+                <input type="text" class="input-large" value="吃饭撑死" maxlength="20" id="nickname">
+
+            </div>
+
+
+            <div class="tn-form-row clearfix">
+
+                <p><label class="tn-form-label">
+
+                    旧密码</label></p>
+
+                <input type="password" class="input-large" value="请输入旧密码" defaultvalue="请输入旧密码" maxlength="20" id="oldpwd">
+                <span style="position:relative" class="noticed2">不修改密码则不需要填此项<s class="triangle"></s></span>
+            </div>
+
+            <div class="tn-form-row clearfix">
+
+                <p><label class="tn-form-label">
+
+                    新密码</label></p>
+
+                <input type="password" class="inputxt" value="请输入新密码" defaultvalue="请输入新密码" id="newpwd">
+               <span style="position:relative" class="noticed2"> 修改密码请先输入旧密码<s class="triangle"></s></span>
+            </div>
+
+        </div>
+</div>
+
+    </div>
+
+    <div class="save_pros"><a class="blue-button" id="submits" href="javascript:void(0);">保存</a></div>
+</div>
+
+
+                
+            </div>
+        </div>
+        
+    </div>
+    
+</div>
